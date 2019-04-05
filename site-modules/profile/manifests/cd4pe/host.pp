1cd7ad062c801eb133c2a617c5ee5409a8003a1b
Rosalind Benoit <rosalind.benoit@puppet.com>
2019-04-04 22:59:20 -0700

    add cd4pe class and image param
    Prior to this commit an include statement was used to initialize default value for all params

--- site-modules/profile/manifests/cd4pe/host.pp
+++ site-modules/profile/manifests/cd4pe/host.pp
@@ -1,11 +1,12 @@
-class profile::cd4pe::host (
-  ) {
-    contain cd4pe
-
-    service { 'discovery.service':
-      ensure => stopped,
-      enable => false,
-      before => Class['cd4pe'],
-    }
+class profile::cd4pe::host {
+  class { 'cd4pe':
+      cd4pe_image => 'puppet/continuous-delivery-for-puppet-enterprise',
   }
+    
+  service { 'discovery.service':
+    ensure => stopped,
+    enable => false,
+    before => Class['cd4pe'],
+  }
+}
   
