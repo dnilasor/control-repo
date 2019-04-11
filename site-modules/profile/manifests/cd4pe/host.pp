d543fd25d6a3ed226e6f6263ec28f3077bcf3937
dnilasor <rosalind@puppet.com>
2019-04-11 14:22:33 -0700

    remove breaking firewall chgs

--- site-modules/profile/manifests/cd4pe/host.pp
+++ site-modules/profile/manifests/cd4pe/host.pp
@@ -1,5 +1,4 @@
 class profile::cd4pe::host {
-  include ::firewalld
 
   class { 'cd4pe':
       cd4pe_image => 'puppet/continuous-delivery-for-puppet-enterprise',
@@ -11,11 +10,5 @@ class profile::cd4pe::host {
     before => Class['cd4pe'],
   }
   
-  firewalld_rich_rule {'allow cd4pe traffic':
-    ensure => present,
-    zone => 'public',
-    source => '172.18.0/16',
-    action => 'accept',
-  }
 }
   
